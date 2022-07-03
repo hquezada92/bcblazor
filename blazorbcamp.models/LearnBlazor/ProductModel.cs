3e7450d75fed2ba245a33bc13bfcac8d6f77d196
Alex Quezada <alex.hquezada92@gmail.com>
2022-07-03 10:27:57 -0600

    Add models project and blazor binding properties

---
+++ blazorbcamp.models/LearnBlazor/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazorbcamp.models.LearnBlazor;
+
+public class ProductModel
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; } = 0.00;
+    public bool IsActive { get; set; }
+}
